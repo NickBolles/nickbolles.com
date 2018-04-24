c4e95822a52c3c649b19d04f04b04987abc49763
NickBolles <nickbolles@live.com>
2018-04-24 00:01:48 -0500

    Add Assets

---
+++ assets/TypeGuards.ts
@@ -0,0 +1,214 @@
+// todo: make these actual type guards
+
+// import * as ObjectId from "bson/lib/bson/objectid";
+const toString: () => string = Object.prototype.toString;
+
+export function isArray<T>(obj: any): obj is T[] {
+  return Array.isArray(obj);
+}
+
+/**
+ * @ngdoc function
+ * @name isUndefined
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is undefined.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is undefined.
+ */
+export function isUndefined(value: any): value is undefined {
+  return typeof value === "undefined";
+}
+
+
+/**
+ * @ngdoc function
+ * @name isDefined
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is defined.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is defined.
+ */
+export function isDefined(value: any): value is undefined {
+  return typeof value !== "undefined";
+}
+
+
+/**
+ * @ngdoc function
+ * @name isObject
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is an `Object`. Unlike `typeof` in JavaScript, `null`s are not
+ * considered to be objects. Note that JavaScript arrays are objects.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is an `Object` but not `null`.
+ */
+export function isObject(value: any): value is Object {
+  // http://jsperf.com/isobject4
+  return value !== null && typeof value === "object";
+}
+
+
+/**
+ * Determine if a value is an object with a null prototype
+ *
+ * @returns {boolean} True if `value` is an `Object` with a null prototype
+ */
+export function isBlankObject(value: any): boolean {
+  return value !== null && typeof value === "object" && !Object.getPrototypeOf(value);
+}
+
+
+/**
+ * @ngdoc function
+ * @name isString
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is a `String`.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is a `String`.
+ */
+export function isString(value: any): value is string {
+  return typeof value === "string";
+}
+
+
+/**
+ * @ngdoc function
+ * @name isNumber
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is a `Number`.
+ *
+ * This includes the "special" numbers `NaN`, `+Infinity` and `-Infinity`.
+ *
+ * If you wish to exclude these then you can use the native
+ * [`isFinite"](https://goo.gl/IOjQXK)
+ * method.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is a `Number`.
+ */
+export function isNumber(value: any): value is number {
+  return typeof value === "number";
+}
+
+
+/**
+ * @ngdoc function
+ * @name isDate
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a value is a date.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is a `Date`.
+ */
+export function isDate(value: any): value is Date {
+  return toString.call(value) === "[object Date]";
+}
+
+
+/**
+ * @ngdoc function
+ * @name isFunction
+ * @module ng
+ * @kind function
+ *
+ * @description
+ * Determines if a reference is a `Function`.
+ *
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is a `Function`.
+ */
+export function isFunction(value: any): value is Function {
+  return typeof value === "function";
+}
+
+
+/**
+ * Determines if a value is a regular expression object.
+ *
+ * @private
+ * @param {*} value Reference to check.
+ * @returns {boolean} True if `value` is a `RegExp`.
+ */
+export function isRegExp(value: any): boolean {
+  return toString.call(value) === "[object RegExp]";
+}
+
+
+/**
+ * Checks if `obj` is a window object.
+ *
+ * @private
+ * @param {*} obj Object to check
+ * @returns {boolean} True if `obj` is a window obj.
+ */
+export function isWindow(obj: any): obj is Window {
+  return obj && obj.window === obj;
+}
+
+
+export function isScope(obj: any): boolean {
+  return obj && obj.$evalAsync && obj.$watch;
+}
+
+
+export function isFile(obj: any): obj is File {
+  return toString.call(obj) === "[object File]";
+}
+
+
+export function isFormData(obj: any): boolean {
+  return toString.call(obj) === "[object FormData]";
+}
+
+
+export function isBlob(obj: any): boolean {
+  return toString.call(obj) === "[object Blob]";
+}
+
+
+export function isBoolean(value: any): value is boolean {
+  return typeof value === "boolean";
+}
+
+
+export function isPromiseLike(obj: any): obj is Promise<any> {
+  return obj && isFunction(obj.then);
+}
+
+
+const TYPED_ARRAY_REGEXP: RegExp =
+  /^\[object (?:Uint8|Uint8Clamped|Uint16|Uint32|Int8|Int16|Int32|Float32|Float64)Array]$/;
+export function isTypedArray(value: any): boolean {
+  return value && isNumber(value.length) && TYPED_ARRAY_REGEXP.test(toString.call(value));
+}
+
+export function isArrayBuffer(obj: any): boolean {
+  return toString.call(obj) === "[object ArrayBuffer]";
+}
+
+
+// export function isObjectId(n): n is ObjectId {
+//   return ObjectId.isValid(n);
+// }
---
+++ assets/skills/skills.ts
@@ -0,0 +1,282 @@
+
+import * as moment from "moment";
+
+export enum SkillType {
+    Platform = 0,
+    Build_Tool = 0,
+    Tool = 0,
+    Language = 3,
+    Framework = 4,
+    Library = 5
+}
+
+export type SkillTypes = keyof SkillType;
+
+export enum Statuses {
+    Idea = "Idea",
+    Designing = "Designing",
+    On_The_Back_Burner = "On the Back Burner",
+    In_Progress = "In Progress",
+    Completed = "Completed",
+    Obsolete = "Obsolete"
+}
+
+export interface ISkill {
+    file: string;
+    name: string;
+    type: SkillType;
+    url: string;
+}
+
+export interface ISkillList {
+    [i: number]: ISkill;
+}
+
+export const Angular: ISkill = {
+    file: "angular.svg",
+    name: "Angular",
+    type: SkillType.Framework,
+    url: "https://angular.io/"
+};
+export const AngularJS: ISkill = {
+    file: "angularjs.svg",
+    name: "Angular JS",
+    type: SkillType.Framework,
+    url: "https://angularjs.org/"
+};
+export const AngularMaterial: ISkill = {
+    file: "angularmaterial.svg",
+    name: "Angular Material 1.x",
+    type: SkillType.Library,
+    url: "https://material.angularjs.org/"
+};
+export const AngularMaterial2: ISkill = {
+    file: "angularmaterial.svg",
+    name: "Angular Material 2",
+    type: SkillType.Library,
+    url: "https://material.angular.io/"
+};
+export const Babel: ISkill = {
+    file: "babel.svg",
+    name: "Babel",
+    type: SkillType.Build_Tool,
+    url: "https://babeljs.io/"
+};
+export const BitBucket: ISkill = {
+    file: "bitbucket.svg",
+    name: "BitBucket",
+    type: SkillType.Tool,
+    url: "https://bitbucket.org/"
+};
+export const Bower: ISkill = {
+    file: "bitbucket.svg",
+    name: "BitBucket",
+    type: SkillType.Tool,
+    url: "https://bitbucket.org/"
+};
+export const Cordova: ISkill = {
+    file: "cordova.svg",
+    name: "Cordova",
+    type: SkillType.Platform,
+    url: "http://cordova.apache.org/"
+};
+export const CSS3: ISkill = {
+    file: "CSS3.svg",
+    name: "CSS3",
+    type: SkillType.Language,
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS"
+};
+export const D3: ISkill = {
+    file: "d3.svg",
+    name: "D3 JS",
+    type: SkillType.Library,
+    url: "https://d3js.org/"
+};
+export const Docker: ISkill = {
+    file: "docker.svg",
+    name: "Docker",
+    type: SkillType.Build_Tool,
+    url: "https://www.docker.com/"
+};
+export const ExpressJS: ISkill = {
+    file: "express.svg",
+    name: "Express JS",
+    type: SkillType.Framework,
+    url: "http://expressjs.com/"
+};
+export const Git: ISkill = {
+    file: "git.svg",
+    name: "Git",
+    type: SkillType.Tool,
+    url: "https://git-scm.com/"
+};
+export const GSAP: ISkill = {
+    file: "greensock.png",
+    name: "Greensock Animation Platform",
+    type: SkillType.Library,
+    url: "https://greensock.com/"
+};
+export const Gulp: ISkill = {
+    file: "gulp.svg",
+    name: "Gulp JS",
+    type: SkillType.Build_Tool,
+    url: "https://gulpjs.com/"
+};
+export const HTML5: ISkill = {
+    file: "HTML5.svg",
+    name: "HTML 5",
+    type: SkillType.Language,
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
+};
+export const Jasmine: ISkill = {
+    file: "jasmine.svg",
+    name: "Jasmine",
+    type: SkillType.Language,
+    url: "https://jasmine.github.io/"
+};
+export const JS: ISkill = {
+    file: "javascript.svg",
+    name: "Javascript",
+    type: SkillType.Language,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+};
+export const Jest: ISkill = {
+    file: "jest.svg",
+    name: "Jest",
+    type: SkillType.Tool,
+    url: "https://facebook.github.io/jest/"
+};
+export const Karma: ISkill = {
+    file: "karma.svg",
+    name: "Karma",
+    type: SkillType.Tool,
+    url: "http://karma-runner.github.io/2.0/index.html"
+};
+export const Mongodb: ISkill = {
+    file: "mongodb.svg",
+    name: "MongoDB",
+    type: SkillType.Language,
+    url: "https://www.mongodb.com/"
+};
+export const NodeJS: ISkill = {
+    file: "nodejs.png",
+    name: "Node JS",
+    type: SkillType.Language,
+    url: "https://nodejs.org/en/"
+};
+export const NPM: ISkill = {
+    file: "npm.svg",
+    name: "NPM",
+    type: SkillType.Tool,
+    url: "https://www.npmjs.com/"
+};
+export const Nuxt: ISkill = {
+    file: "nuxt.svg",
+    name: "Nuxt",
+    type: SkillType.Framework,
+    url: "https://www.nuxtjs.com/"
+};
+export const Prettier: ISkill = {
+    file: "prettier.svg",
+    name: "Prettier",
+    type: SkillType.Tool,
+    url: "https://prettier.io/"
+};
+export const React: ISkill = {
+    file: "react.png",
+    name: "React",
+    type: SkillType.Framework,
+    url: "https://prettier.io/"
+};
+export const RXJS: ISkill = {
+    file: "rxjs.png",
+    name: "RxJs",
+    type: SkillType.Library,
+    url: "http://reactivex.io/rxjs/"
+};
+export const SASS: ISkill = {
+    file: "sass-logo.png",
+    name: "SASS",
+    type: SkillType.Language,
+    url: "http://sass-lang.com/"
+};
+export const Typescript: ISkill = {
+    file: "typescript.svg",
+    name: "Typescript",
+    type: SkillType.Language,
+    url: "http://www.typescriptlang.org/"
+};
+export const VSCode: ISkill = {
+    file: "vscode.svg",
+    name: "Visual Studio Code",
+    type: SkillType.Tool,
+    url: "https://code.visualstudio.com/"
+};
+export const Vue: ISkill = {
+    file: "vuejs.svg",
+    name: "Vue JS",
+    type: SkillType.Framework,
+    url: "https://vuejs.org/"
+};
+export const Vuetify: ISkill = {
+    file: "vuetify.svg",
+    name: "Vuetify JS",
+    type: SkillType.Library,
+    url: "https://vuetifyjs.com/en/"
+};
+export const WebPack: ISkill = {
+    file: "webpack.png",
+    name: "Webpack",
+    type: SkillType.Build_Tool,
+    url: "https://webpack.js.org/"
+};
+export const Webstorm: ISkill = {
+    file: "webstorm.svg",
+    name: "Webstorm",
+    type: SkillType.Tool,
+    url: "https://www.jetbrains.com/webstorm/"
+};
+export const Yarn: ISkill = {
+    file: "yarn.svg",
+    name: "Yarn",
+    type: SkillType.Tool,
+    url: "https://yarnpkg.com/lang/en/docs/install/"
+};
+
+export const Skills: ISkill[] = [
+    Angular,
+    AngularJS,
+    AngularMaterial,
+    AngularMaterial2,
+    Babel,
+    BitBucket,
+    Bower,
+    Cordova,
+    CSS3,
+    D3,
+    Docker,
+    ExpressJS,
+    Git,
+    GSAP,
+    Gulp,
+    HTML5,
+    Jasmine,
+    JS,
+    Jest,
+    Karma,
+    Mongodb,
+    NodeJS,
+    NPM,
+    Nuxt,
+    Prettier,
+    React,
+    RXJS,
+    SASS,
+    Typescript,
+    VSCode,
+    Vue,
+    Vuetify,
+    WebPack,
+    Webstorm,
+    Yarn
+];
