828618bdf6c62a0644de9d56020305e5ee9059f4
NickBolles <nickbolles@live.com>
2018-09-09 01:09:12 -0500

    rename html to old

--- server.js
+++
@@ -1,6 +0,0 @@
-var http=require('http-server')
-
-http.createServer({root: './vue', port: 8080});
-//http.createServer({root: './react', port: 8081});
-http.createServer({root: './html', port: 8082});
-console.log("servers running")
