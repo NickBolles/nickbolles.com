e32519239eae084fbcdbd285d2f18589da4a9603
NickBolles <nickbolles@live.com>
2018-12-12 00:39:11 -0600

    fix resumes, use v-img for more reliable loading

--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -1,15 +1,40 @@
 <template>
-  <v-layout column align-stretch v-bind:class="loaded ? 'loaded':'loading'" v-resize="onResize">
-    <v-layout column align-center class="nb main landing section application theme--dark" v-bind:style="contentStyle" align>
+  <v-layout
+    column
+    align-stretch
+    v-bind:class="loaded ? 'loaded':'loading'"
+    v-resize="onResize"
+  >
+    <v-layout
+      column
+      align-center
+      class="nb main landing section application theme--dark"
+      v-bind:style="contentStyle"
+      align
+    >
       <div class="pb-5 px-5 nb main avatar">
-        <v-avatar v-bind:size="avatarHeight + 'px'" v-parallax="parallaxMult">
-          <v-img src="/NickBolles.jpg" srcSet="/NickBolles.webp 2x" :style="avatarStyle" alt="An Awesome Picture of me" onLoad="onLoaded">
+        <v-avatar
+          v-bind:size="avatarHeight + 'px'"
+          v-parallax="parallaxMult"
+        >
+          <v-img
+            src="/NickBolles.jpg"
+            srcSet="/NickBolles.webp 2x"
+            :style="avatarStyle"
+            alt="An Awesome Picture of me"
+            @load="onLoaded"
+          >
             <div class="fill-height bottom-gradient"></div>
           </v-img>
           <!-- <img src="/main-square.webp" alt="An Awesome Picture of me" v-bind:style="avatarStyle"> -->
         </v-avatar>
       </div>
-      <v-layout class="nb main intro" column align-center justify-center>
+      <v-layout
+        class="nb main intro application theme--dark"
+        column
+        align-center
+        justify-center
+      >
         <p class="display-2 pa-3 text-xs-center">
           Hey! I'm Nick
         </p>
@@ -24,20 +49,58 @@
         </p>
       </v-layout>
     </v-layout>
-    <v-layout row justify-center ref="linksList" class="nb main links application theme--dark pa-2">
-      <v-container v-bind="{ [`grid-list-sm`]: $vuetify.breakpoint.mdAndUp, [`grid-list-xs`]: $vuetify.breakpoint.smAndDown }" 
-              v-sw-scene="{triggerHook:0}" v-sw-pin="{}" 
-              class="application theme--dark" justify-center>
-        <v-layout row wrap justify-space-around>
-          <v-btn to="/" nuxt>About Me</v-btn>
-          <v-btn to="/myExperience" nuxt color="primary">Experience</v-btn>
-          <v-btn to="/myKnowledge" nuxt color="primary">Knowledge</v-btn>
-          <v-btn to="/myProjects" nuxt color="primary">Projects</v-btn>
-          <v-btn to="/contactMe" nuxt color="primary">Contact Me</v-btn>
+    <v-layout
+      row
+      justify-center
+      align-center
+      ref="linksList"
+      class="nb main links application theme--dark pa-2"
+    >
+      <v-container
+        v-bind="{ [`grid-list-sm`]: $vuetify.breakpoint.mdAndUp, [`grid-list-xs`]: $vuetify.breakpoint.smAndDown }"
+        v-sw-scene="{triggerHook:0}"
+        v-sw-pin="{}"
+        justify-center
+      >
+        <v-layout
+          row
+          wrap
+          justify-space-around
+        >
+          <v-btn
+            to="/"
+            nuxt
+            color="primary"
+          >About Me</v-btn>
+          <v-btn
+            to="/myExperience"
+            nuxt
+            color="primary"
+          >Experience</v-btn>
+          <v-btn
+            to="/myKnowledge"
+            nuxt
+            color="primary"
+          >Knowledge</v-btn>
+          <v-btn
+            to="/myProjects"
+            nuxt
+            color="primary"
+          >Projects</v-btn>
+          <v-btn
+            to="/contactMe"
+            nuxt
+            color="primary"
+          >Contact Me</v-btn>
         </v-layout>
       </v-container>
     </v-layout>
-    <v-layout class="nb content section" v-bind:style="contentStyle" ref="content" flex>
+    <v-layout
+      class="nb content section application theme--dark"
+      v-bind:style="contentStyle"
+      ref="content"
+      flex
+    >
       <v-layout>
         <transition name="fade-transition">
           <nuxt-child></nuxt-child>
@@ -51,278 +114,279 @@
 </template>
 
 <script lang="ts">
-import "material-design-icons-iconfont/dist/material-design-icons.css";
-import Vue from "vue";
-import Component from "nuxt-class-component";
-import {
-  Emit,
-  Inject,
-  Model,
-  Prop,
-  Provide,
-  Watch
-} from "vue-property-decorator";
-import { TweenLite, TimelineLite, Sine } from "gsap";
-import * as scrollTo from "../node_modules/gsap/src/uncompressed/plugins/ScrollToPlugin";
-import { Route } from "vue-router/types/router";
+  import "material-design-icons-iconfont/dist/material-design-icons.css";
+  import Vue from "vue";
+  import Component from "nuxt-class-component";
+  import {
+    Emit,
+    Inject,
+    Model,
+    Prop,
+    Provide,
+    Watch
+  } from "vue-property-decorator";
+  import { TweenLite, TimelineLite, Sine } from "gsap";
+  import * as scrollTo from "../node_modules/gsap/src/uncompressed/plugins/ScrollToPlugin";
+  import { Route } from "vue-router/types/router";
 
-@Component({})
-export default class MainComponent extends Vue {
-  static meta = {
-    scrollTo: "#nb-content-bottom-anchor"
-  };
-
-  $refs: {
-    linksList: HTMLElement;
-    content: HTMLElement;
-  };
-  loaded = false;
-
-  timeline: TimelineLite = new TimelineLite();
-  staggerDelay = 0.3;
-  animDur = 2;
-  parallaxMult = 0.3;
-
-  linksHeight: number = 0;
-  avatarHeight: number = 0;
-
-  get avatarStyle(): any {
-    return {
-      marginTop: `${-1 * this.parallaxMult * this.avatarHeight}px`
+  @Component({})
+  export default class MainComponent extends Vue {
+    static meta = {
+      scrollTo: "#nb-content-bottom-anchor"
     };
-  }
 
-  get contentStyle(): any {
-    return {
-      height: `calc(100vh - ${this.linksHeight}px)`
+    $refs: {
+      linksList: HTMLElement;
+      content: HTMLElement;
     };
-  }
+    loaded = false;
 
-  //#region Vue Hooks
-  mounted(): void {
-    this.preLoadImg();
-    this.setupTimeline();
+    timeline: TimelineLite = new TimelineLite();
+    staggerDelay = 0.3;
+    animDur = 2;
+    parallaxMult = 1.2;
 
-    this.onResize();
-    this.onRouteUpdate(this.$route);
-  }
+    linksHeight: number = 0;
+    avatarHeight: number = 0;
 
-  @Watch("$route")
-  onRouteUpdate(to?: Route, from?: Route): void {
-    this.updateSectionHeight();
-    this.updateScroll(to);
-  }
-  //#endregion
+    get avatarStyle(): any {
+      return {
+        marginTop: `${-1 * this.parallaxMult * 2 * this.avatarHeight}px`
+      };
+    }
 
-  //#region event handlers
+    get introStyle(): any {
+      return {
+        boxShadow: `0px -21px 66px -4px ${this.$vuetify.theme.primary}`
+      };
+    }
 
-  onLoaded(): void {
-    this.loaded = true;
-    this.timeline.delay(0.5).play();
-  }
+    get contentStyle(): any {
+      return {
+        height: `calc(100vh - ${this.linksHeight}px)`
+      };
+    }
 
-  onResize(): void {
-    this.updateSectionHeight();
-    this.updateScroll();
-    this.updateAvatarHeight();
-  }
-  //#endregion
+    //#region Vue Hooks
+    mounted(): void {
+      this.setupTimeline();
+      this.onResize();
+      this.onRouteUpdate(this.$route);
+    }
 
-  //#region Methods
-  //#region Init Methods
-  private preLoadImg(): void {
-    let img: HTMLImageElement = new Image();
-    img.onload = () => this.onLoaded();
-    img.src = "/main-square.webp";
-  }
+    @Watch("$route")
+    onRouteUpdate(to?: Route, from?: Route): void {
+      this.updateSectionHeight();
+      this.updateScroll(to);
+    }
+    //#endregion
 
-  private setupTimeline(): void {
-    this.timeline.pause();
-    this.timeline.fromTo(
-      ".nb.main.avatar",
-      this.animDur,
-      { opacity: 0, transform: "translateY(-50px)" },
-      { opacity: 1, transform: "translateY(0px)", ease: Sine.easeInOut },
-      0
-    );
-    this.timeline.staggerFromTo(
-      ".nb.main.intro p",
-      this.animDur,
-      { opacity: 0, transform: "translateY(20px)" },
-      { opacity: 1, transform: "translateY(0px)", ease: Sine.easeInOut },
-      this.staggerDelay,
-      this.staggerDelay
-    );
-    this.timeline.staggerTo(
-      ".nb.main.links a",
-      this.animDur / 3,
-      { opacity: 1, transform: "translateY(0px)", ease: Sine.easeOut },
-      this.staggerDelay / 2
-    );
-  }
-  //#endregion
+    //#region event handlers
 
-  private updateSectionHeight(): void {
-    const height: number = this.$refs.linksList.clientHeight;
-    if (height !== this.linksHeight) {
-      this.linksHeight = height;
+    onLoaded(): void {
+      this.loaded = true;
+      this.timeline.delay(0.5).play();
+    }
+
+    onResize(): void {
+      this.updateSectionHeight();
       this.updateScroll();
+      this.updateAvatarHeight();
     }
-  }
-  private updateScroll(to: Route = this.$route): void {
-    let offset: number = 0;
-    if (to && to.name) {
-      let parts: string[] = to.name.split("-");
-      if (parts.length === 2 && parts[0] === "index") {
-        offset = this.$refs.linksList.offsetTop;
-      }
-    }
-    TweenLite.to(document.scrollingElement, 0.5, {
-      scrollTop: offset,
-      ease: Sine.easeInOut
-    });
-    if (offset !== 0) {
+    //#endregion
+
+    //#region Methods
+    //#region Init Methods
+
+    private setupTimeline(): void {
+      this.timeline.pause();
+      this.timeline.fromTo(
+        ".nb.main.avatar",
+        this.animDur,
+        { opacity: 0, transform: "translateY(-50px)" },
+        { opacity: 1, transform: "translateY(0px)", ease: Sine.easeInOut },
+        0
+      );
+      this.timeline.staggerFromTo(
+        ".nb.main.intro p",
+        this.animDur,
+        { opacity: 0, transform: "translateY(20px)" },
+        { opacity: 1, transform: "translateY(0px)", ease: Sine.easeInOut },
+        this.staggerDelay,
+        this.staggerDelay
+      );
       this.timeline.staggerTo(
         ".nb.main.links a",
         this.animDur / 3,
         { opacity: 1, transform: "translateY(0px)", ease: Sine.easeOut },
-        this.staggerDelay / 2,
-        0
+        this.staggerDelay / 2
       );
     }
-  }
+    //#endregion
 
-  private updateAvatarHeight(): void {
-    const percent: number = this.$vuetify.breakpoint.xs ? 33 : 50;
-    this.avatarHeight = this.$vuetify.breakpoint.height * (percent / 100);
+    private updateSectionHeight(): void {
+      const height: number = this.$refs.linksList.clientHeight;
+      if (height !== this.linksHeight) {
+        this.linksHeight = height;
+        this.updateScroll();
+      }
+    }
+    private updateScroll(to: Route = this.$route): void {
+      let offset: number = 0;
+      if (to && to.name) {
+        let parts: string[] = to.name.split("-");
+        if (parts.length === 2 && parts[0] === "index") {
+          offset = this.$refs.linksList.offsetTop;
+        }
+      }
+      TweenLite.to(document.scrollingElement, 0.5, {
+        scrollTop: offset,
+        ease: Sine.easeInOut
+      });
+      if (offset !== 0) {
+        this.timeline.staggerTo(
+          ".nb.main.links a",
+          this.animDur / 3,
+          { opacity: 1, transform: "translateY(0px)", ease: Sine.easeOut },
+          this.staggerDelay / 2,
+          0
+        );
+      }
+    }
+
+    private updateAvatarHeight(): void {
+      const percent: number = this.$vuetify.breakpoint.xs ? 33 : 50;
+      this.avatarHeight = this.$vuetify.breakpoint.height * (percent / 100);
+    }
+    //#endregion
   }
-  //#endregion
-}
 </script>
 
 <style lang="scss">
-html {
-  // overflow: hidden;
-  font-size: 16px;
-}
-
-a {
-  text-decoration: none;
-}
-
-.loading {
-  visibility: hidden;
-}
-
-.nb {
-  &.section {
-    height: 100vh;
-    max-height: 100vh;
-    width: 100%;
-    max-width: 1440px;
-    transition: height, width, max-height,
-      max-width 0.25s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  html {
+    // overflow: hidden;
+    font-size: 16px;
   }
 
-  &.main {
-    z-index: 3;
-    width: 100%;
-    max-width: 800px;
+  a {
+    text-decoration: none;
+  }
 
-    &.avatar {
-      text-align: center;
-      .v-avatar {
-        position: relative;
-        top: -56px;
-      }
-      * {
-        transition: all 0.25s cubic-bezier(0.445, 0.05, 0.55, 0.95),
-          transform 1ms;
-      }
-    }
+  .loading {
+    visibility: hidden;
+  }
 
-    &.landing {
-      align-self: center;
+  .nb {
+    &.section {
+      height: 100vh;
+      max-height: 100vh;
       width: 100%;
+      max-width: 1440px;
+      transition: height, width, max-height,
+        max-width 0.25s cubic-bezier(0.445, 0.05, 0.55, 0.95);
     }
 
-    &.intro {
-      z-index: 5;
+    &.main {
+      z-index: 3;
       width: 100%;
-      overflow: auto;
-    }
+      max-width: 800px;
 
-    &.links {
-      min-width: 48px;
-      width: 100%;
-      a {
-        opacity: 0;
+      &.avatar {
+        text-align: center;
+        .v-avatar {
+          position: relative;
+          top: -56px;
+        }
+        * {
+          transition: all 0.25s cubic-bezier(0.445, 0.05, 0.55, 0.95),
+            transform 1ms;
+        }
+      }
+
+      &.landing {
+        align-self: center;
+        width: 100%;
+      }
+
+      &.intro {
+        z-index: 5;
+        width: 100%;
+        overflow: auto;
+        box-shadow: 0px -25px 50px 15px #303030;
+      }
+
+      &.links {
+        min-width: 48px;
+        width: 100%;
+        align-self: center;
+        a {
+          opacity: 0;
+        }
       }
     }
+    &.content {
+      z-index: 3;
+      overflow-y: auto;
+    }
   }
-  &.content {
-    z-index: 2;
-    overflow-y: auto;
-  }
-}
 
-.body-1,
-.body-2 {
-  font-size: 16px !important;
-}
-
-.scrollwizardry-pin-spacer {
-  width: 100%;
-  z-index: 100;
-}
-
-@media screen and (max-width: 600px) {
-  .display-3 {
-    font-size: 28px !important;
-    font-weight: 400;
-    line-height: 1 !important;
-    letter-spacing: -0.02em !important;
-    padding: 6px !important;
-  }
-  .display-2 {
-    font-size: 24px !important;
-    line-height: 48px !important;
-  }
-  .headline {
+  .body-1,
+  .body-2 {
     font-size: 16px !important;
-    font-weight: 400;
-    line-height: 32px !important;
-    letter-spacing: normal !important;
   }
-  p {
-    margin: 8px;
-  }
-  .pa1 {
-    padding: 2px !important;
-  }
-  .pa2 {
-    padding: 4px !important;
-  }
-  .pa3 {
-    padding: 8px !important;
-  }
-}
 
-// Breakpoints for the buttons to all fit
-@media screen and (max-width: 700) {
-  .links {
-    padding: 0;
-    .container {
-      padding: 4px 0 0;
+  .scrollwizardry-pin-spacer {
+    width: 100%;
+    z-index: 100;
+  }
+
+  @media screen and (max-width: 600px) {
+    .display-3 {
+      font-size: 28px !important;
+      font-weight: 400;
+      line-height: 1 !important;
+      letter-spacing: -0.02em !important;
+      padding: 6px !important;
     }
-    .btn {
-      min-width: initial;
-      font-size: 12px;
-      margin: 4px 2px;
+    .display-2 {
+      font-size: 24px !important;
+      line-height: 48px !important;
     }
-    .btn__content {
-      padding: 0 8px;
+    .headline {
+      font-size: 16px !important;
+      font-weight: 400;
+      line-height: 32px !important;
+      letter-spacing: normal !important;
+    }
+    p {
+      margin: 8px;
+    }
+    .pa1 {
+      padding: 2px !important;
+    }
+    .pa2 {
+      padding: 4px !important;
+    }
+    .pa3 {
+      padding: 8px !important;
     }
   }
-}
+
+  // Breakpoints for the buttons to all fit
+  @media screen and (max-width: 700) {
+    .links {
+      padding: 0;
+      .container {
+        padding: 4px 0 0;
+      }
+      .btn {
+        min-width: initial;
+        font-size: 12px;
+        margin: 4px 2px;
+      }
+      .btn__content {
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
--- vue/pages/index/myExperience.vue
+++ vue/pages/index/myExperience.vue
@@ -1,68 +1,120 @@
 <template>
-  <v-layout id="experience" column align-center justify-center>
-    <v-flex></v-flex>
+  <v-layout
+    id="experience"
+    style="width: 100%;"
+    column
+    align-center
+    justify-center
+  >
     <h1>
       Check me out elsewhere on the web
     </h1>
-    <v-layout>
-      <v-flex>
-        <a href="/Nicholas Bolles Resume - Latest.pdf" class="btn nb ext-link">
-          <img src="~/assets/style/file-document.svg" alt="Resume Icon" />
-          <h1 class="title">Resume</h1>
-        </a>
-      </v-flex>
-      <v-flex>
-        <a href="https://www.linkedin.com/in/nickbolles" class="btn nb ext-link">
-          <img src="~/assets/style/linkedin-box.svg" alt="Linked Icon" />
-          <h1 class="title">Linked In</h1>
-        </a>
-      </v-flex>
-      <v-flex>
-        <a href="https://github.com/NickBolles" class="btn nb ext-link">
-          <img src="~/assets/style/github-box.svg" alt="Github Icon" />
-          <h1 class="title">Github</h1>
-        </a>
-      </v-flex>
+    <v-layout
+      row
+      class="py-5 no-flex"
+    >
+      <v-responsive
+        aspect-ratio="1"
+        v-for="link in links"
+        :key="link.title"
+      >
+        <v-btn
+          large
+          :href="link.href"
+          class="btn nb ext-link darken-2"
+          color="primary"
+        >
+          <v-layout
+            column
+            style="align-items: center;"
+          >
+            <img
+              :src="link.icon"
+              :alt="link.alt"
+            />
+            <h1 class="title">{{link.title}}</h1>
+          </v-layout>
+        </v-btn>
+      </v-responsive>
     </v-layout>
-    <v-flex></v-flex>
 
-    <v-layout column align-center justify-center>
+    <v-layout
+      column
+      align-center
+      justify-center
+      class="no-flex"
+    >
       <h4>Also check out</h4>
-      <v-layout align-center justify-center align-content-center="">
-        <v-btn to="/myKnowledge" nuxt color="primary">Knowledge</v-btn>
+      <v-layout
+        align-center
+        justify-center
+        align-content-center=""
+      >
+        <v-btn
+          to="/myKnowledge"
+          nuxt
+          color="primary"
+        >Knowledge</v-btn>
         <v-flex>and</v-flex>
-        <v-btn to="/myProjects" nuxt color="primary">Projects</v-btn>
+        <v-btn
+          to="/myProjects"
+          nuxt
+          color="primary"
+        >Projects</v-btn>
       </v-layout>
     </v-layout>
-    <v-flex></v-flex>
 
   </v-layout>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "nuxt-class-component";
+  import Vue from "vue";
+  import Component from "nuxt-class-component";
 
-@Component({})
-export default class MyExperienceComponent extends Vue {}
+  @Component({})
+  export default class MyExperienceComponent extends Vue {
+    links = [
+      {
+        href: "/Nicholas Bolles Resume.pdf",
+        icon: "/file-document.svg",
+        alt: "Resume Icon",
+        title: "Resume"
+      },
+      {
+        href: "https://www.linkedin.com/in/nickbolles",
+        icon: "/linkedin-box.svg",
+        alt: "Linked Icon",
+        title: "Linked In"
+      },
+      {
+        href: "https://github.com/NickBolles",
+        icon: "/github-box.svg",
+        alt: "Github Icon",
+        title: "Github"
+      }
+    ];
+  }
 </script>
 
 <style lang="scss" scoped>
-.nb {
-  &.ext-link {
-    padding: 2em;
-    display: flex;
-    flex-direction: column;
-    height: initial;
-    max-width: 25vw;
-    max-height: 25vh;
+  .nb {
+    &.ext-link {
+      padding: 2em;
+      display: flex;
+      flex-direction: column;
+      height: initial;
+      max-width: 25vw;
+      max-height: 25vh;
 
-    img {
-      height: 48px;
+      img {
+        height: 48px;
 
-      max-width: 48px;
-      margin-bottom: 8px;
+        max-width: 48px;
+        margin-bottom: 8px;
+      }
     }
   }
-}
+  .no-flex {
+    flex: none;
+  }
 </style>
