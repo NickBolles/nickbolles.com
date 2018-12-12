d6f921702b3b9b0b308f66186a4f7d034a453b2f
NickBolles <nickbolles@live.com>
2018-12-12 00:28:16 -0600

    fix optimize script to copy pngs too

--- vue/resources/optimizeImages.js
+++ vue/resources/optimizeImages.js
@@ -3,27 +3,35 @@ const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
 const imageminSVG = require('imagemin-svgo');
 const imageminJPG = require('imagemin-mozjpeg');
+const imageminPNG = require('imagemin-pngquant');
 
 
-imagemin([path.join(__dirname, 'images/unoptimized/*.{jpg,png}')], path.join(__dirname,  '../', 'static/'), {
-	use: [
-        imageminWebp({quality: 60, method: 6})
-	]
+imagemin([path.join(__dirname, 'images/unoptimized/*.{jpg,png}')], path.join(__dirname, '../', 'static/'), {
+  use: [
+    imageminWebp({ quality: 60, method: 6 })
+  ]
 }).then(() => {
-	console.log('Images optimized');
+  console.log('webp Images created/optimized');
 });
-imagemin([path.join(__dirname, 'images/unoptimized/*.{jpg,png}')], path.join(__dirname,  '../', 'static/'), {
-	use: [
-		imageminJPG({quality: 60}),
-	]
+imagemin([path.join(__dirname, 'images/unoptimized/*.{jpg,png}')], path.join(__dirname, '../', 'static/'), {
+  use: [
+    imageminJPG({ quality: 60 }),
+  ]
 }).then(() => {
-	console.log('Images optimized');
+  console.log('JPG images optimized');
 });
 
 imagemin([path.join(__dirname, 'images/unoptimized/skill-icons/*.svg')], path.join(__dirname, '../', 'static/skill-icons/'), {
-	use: [
-		imageminSVG()
-	]
+  use: [
+    imageminSVG()
+  ]
 }).then(() => {
-	console.log('Skill Icons optimized');
+  console.log('Skill Icon SVGss optimized');
+});
+imagemin([path.join(__dirname, 'images/unoptimized/skill-icons/*.png')], path.join(__dirname, '../', 'static/skill-icons/'), {
+  use: [
+    imageminPNG()
+  ]
+}).then(() => {
+  console.log('Skill Icon PNGs optimized');
 });
